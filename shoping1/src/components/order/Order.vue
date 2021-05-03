cd302d063d276b75d8b7a12938f84ac438939e2b
syz247179876 <247179876@qq.com>
2021-05-03 21:40:14 +0800

    初步完成后台订单功能

--- shoping1/src/components/order/Order.vue
+++ shoping1/src/components/order/Order.vue
@@ -17,38 +17,94 @@
             placeholder="请输入内容"
             v-model="queryInfo.query"
             clearable
-            @clear="getGoodList"
           >
-            <el-button
-              slot="append"
-              icon="el-icon-search"
-              @click="getGoodList"
-            ></el-button>
+            <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary" @click="addGoods">添加商品</el-button>
+          <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
       <!-- 商品表格 -->
-      <el-table :data="goodList" border stripe>
+      <el-table
+        :data="orderList"
+        border
+        stripe
+        style="width: 100%"
+        :row-class-name="tableRowClassName"
+      >
         <el-table-column type="index"></el-table-column>
+        <!-- 行展开功能 -->
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" block class="demo-table-expand">
+              <el-form-item label="订单号:">
+                <span>{{ props.row.commodity_name }}</span>
+              </el-form-item>
+              <el-form-item label="商品名称:">
+                <span>{{ props.row.commodity_name }}</span>
+              </el-form-item>
+              <el-form-item label="订单状态:">
+                <span>{{ props.row.status }}</span>
+              </el-form-item>
+              <el-form-item label="是否审核:">
+                <span>{{
+                  props.row.is_checked == 'False' ? '未审核' : '已接单'
+                }}</span>
+              </el-form-item>
+              <el-form-item label="订单有效期:">
+                <span>{{ props.row.efficient_time }}</span>
+              </el-form-item>
+              <el-form-item label="收货人:">
+                <span>{{ props.row.recipient }}</span>
+              </el-form-item>
+              <el-form-item label="收货地址:">
+                <span>{{
+                  props.row.province.split('/').join(' ') + props.row.region
+                }}</span>
+              </el-form-item>
+              <el-form-item label="收货人电话:">
+                <span>{{ props.row.phone }}</span>
+              </el-form-item>
+              <el-form-item label="订单商品价格:">
+                <span>{{ props.row.sku_favourable_price }}</span>
+              </el-form-item>
+              <el-form-item label="订单商品属性:">
+                <span>{{ props.row.sku_properties }}</span>
+              </el-form-item>
+              <el-form-item label="订单商品名称:">
+                <span>{{ props.row.sku_name }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="订单号"
+          prop="order_id"
+          width="150px"
+        ></el-table-column>
         <el-table-column
           label="商品名称"
           prop="commodity_name"
-        ></el-table-column>
-        <el-table-column
-          label="商品价格"
-          prop="price"
           width="85px"
         ></el-table-column>
         <el-table-column
-          label="优惠价"
-          prop="favourable_price"
+          label="订单状态"
+          prop="status"
           width="85px"
         ></el-table-column>
-        <el-table-column label="简要介绍" prop="intro" width="190px">
+        <el-table-column
+          label="下单时间"
+          prop="efficient_time"
+          sortable
+          width="170px"
+        >
         </el-table-column>
+        <el-table-column label="sku属性" prop="sku_properties" width="150px">
+        </el-table-column>
+        <el-table-column label="sku名称" prop="sku_name" width="150px">
+        </el-table-column>
+
         <el-table-column label="操作" width="180px">
           <template slot-scope="scope">
             <el-button type="primary" size="mini">编辑</el-button>
@@ -62,7 +118,7 @@
         </el-table-column>
       </el-table>
       <!-- 底部分页 -->
-      <el-pagination
+      <!-- <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="queryInfo.pagenum"
@@ -72,7 +128,7 @@
         :total="count"
         :background="true"
       >
-      </el-pagination>
+      </el-pagination> -->
     </el-card>
   </div>
 </template>
@@ -89,12 +145,23 @@ export default {
         pagesize: 10,
       },
       // 商品列表数据
-      goodList: [],
-      // 商品列表总数
-      total: 0,
+      orderList: [],
       permission: 100002,
       count: 0,
     }
+  },
+  created() {
+    this.getOrderSeller()
+  },
+  methods: {
+    // 异步获取订单数据
+    async getOrderSeller() {
+      const res = await this.$http.get('/order/chsc/apis/order/seller/')
+      if (res.status === 200) {
+        this.orderList = res.data.data
+        this.count = res.data.count
+      }
+    },
   },
 }
 </script>
